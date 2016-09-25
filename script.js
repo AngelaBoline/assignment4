0d863c5513fd92ef8882d12383fa59d833f91b87
AngelaBoline <angelaboline@gmail.com>
2016-09-25 13:26:15 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-document.getElementById('BtnDonate').addEventListener('click', function() {
-  console.log("There are three speeches on the page");
-  //Code in here executes when the user clicks the "Donate" button.
-});
-
-document.getElementById('BtnChurchill').addEventListener('click', function() {
-  console.log("This speech was written by Winston Churchill in 1940. It is false that this year is BCE");
-  //Code in here executes when the user clicks the "Churchill" button.
-
-});
-
-document.getElementById('BtnGhandi').addEventListener('click', function() {
-  console.log("This speech was written by Ghandi in 1942. It is false that this year is B.C.E.");
-  //Code in here executes when the user clicks the "Churchill" button.
-});
-
-document.getElementById('BtnDemosthenes').addEventListener('click', function() {
-  console.log("This speech was written by Demosthenes in 342. It is true that this year is B.C.E.");
-  //Code in here executes when the user clicks the "Churchill" button.
-});
-
 var Churchill = {
   'name': 'Winston Churchill',
   'year': '1940',
@@ -43,3 +22,25 @@ var speeches = ['Churchill', 'Ghandi', 'Demosthenes'];
 var difference = (1942-1940);
 
 console.log("Ghandi's speech and Churchill's speech are " + [difference] + " years apart.");
+
+document.getElementById('BtnDonate').addEventListener('click', function() {
+  console.log("There are three speeches on the page");
+  //Code in here executes when the user clicks the "Donate" button.
+});
+
+document.getElementById('BtnChurchill').addEventListener('click', function() {
+  console.log("This speech was written by Winston Churchill in 1940. It is false that this year is BCE");
+  //Code in here executes when the user clicks the "Churchill" button.
+
+});
+
+document.getElementById('BtnGhandi').addEventListener('click', function() {
+  console.log("This speech was written by Ghandi in 1942. It is false that this year is B.C.E.");
+  //Code in here executes when the user clicks the "Churchill" button.
+});
+
+document.getElementById('BtnDemosthenes').addEventListener('click', function() {
+  console.log("This speech was written by Demosthenes in 342. It is true that this year is B.C.E.");
+  //Code in here executes when the user clicks the "Churchill" button.
+});
+
